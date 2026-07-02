46e346bf6438c3a8d06b0f460b85ef2e2e092906
joaovitor-9 <joaovamorim11@gmail.com>
2026-07-02 01:16:52 -0300

    update

--- Back-end/app/database.py
+++ Back-end/app/database.py
@@ -11,7 +11,13 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("Faltou configurar a DATABASE_URL no arquivo .env!")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    connect_args={"sslmode": "require"}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
