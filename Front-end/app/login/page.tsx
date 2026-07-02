d585699bc7454bd1ede00f40576a395721c53979
joaovitor-9 <joaovamorim11@gmail.com>
2026-07-02 01:09:51 -0300

    update

--- Front-end/app/login/page.tsx
+++ Front-end/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function Login() {
 
     if (tipoLogin === 'cliente') {
       try {
-        const response = await fetch("`${process.env.NEXT_PUBLIC_API_URL || '[http://127.0.0.1:8000](http://127.0.0.1:8000)'}/login`", {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000'}/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email: identificacao, senha: senha }),
