b4a852518b19cfb000d4886ed15188ece962f572
joaovitor-9 <joaovamorim11@gmail.com>
2026-07-02 00:28:38 -0300

    update

--- Back-end/app/main.py
+++ Back-end/app/main.py
@@ -71,7 +71,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000","https://delivery-farmacia-nine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
